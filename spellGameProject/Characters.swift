25a3aed1bb143b33ccbb9c46f202b613eb57ac58
Snigdha Mishra <snigdha.mishra.066@k12.friscoisd.org>
2019-12-13 13:44:01 -0600

    hero and enemy

--- spellGameProject/Characters.swift
+++ spellGameProject/Characters.swift
@@ -13,29 +13,40 @@ class Character {
     var health: Int
     var energy: Int
     var attackDamage: Int
+    var spellCasted: String
     
-    init( health: Int, energy: Int, attackDamage: Int) {
+    init( health: Int, energy: Int, attackDamage: Int, spellCasted: String) {
         self.health = health
         self.energy = energy
         self.attackDamage = attackDamage
+        self.spellCasted = spellCasted
     }
+    
 }
 
 class Hero: Character {
-    
+    var spells: [String] = ["Fire", "Water", "Earth", "Healing"]
     
     init(health: Int, energy: Int) {
-        super.init(health: 100, energy: 100, attackDamage: 10)
+        let num = Int.random(in: 1...4)
+        
+        super.init(health: 100, energy: 100, attackDamage: 10, spellCasted: spells[num])
         
     }
 }
 
 class Enemy: Character {
     
+    var spells: [String] = ["Fire", "Death", "Poison", "Ice"]
     
     init(health: Int, energy: Int) {
-        super.init(health: 100, energy: 100, attackDamage: 10)
+        let num = Int.random(in: 1...4)
         
-    }
+        super.init(health: 100, energy: 100, attackDamage: 10, spellCasted: spells[num])
+        
+        }
     
-}
+    
+    }
+
+
