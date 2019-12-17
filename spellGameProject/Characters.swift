bb5217f0a02148100d8cf303b9688f4976f68fa4
Snigdha Mishra <snigdha.mishra.066@k12.friscoisd.org>
2019-12-17 14:16:51 -0600

    testing

--- spellGameProject/Characters.swift
+++ spellGameProject/Characters.swift
@@ -12,36 +12,36 @@ class Character {
     var health: Int
     var energy: Int
     var attackDamage: Int
-    var spellCasted: String
+   // var spellCasted: String
     
-    init( health: Int, energy: Int, attackDamage: Int, spellCasted: String) {
+    init( health: Int, energy: Int, attackDamage: Int) {
         self.health = health
         self.energy = energy
         self.attackDamage = attackDamage
-        self.spellCasted = spellCasted
+       // self.spellCasted = spellCasted
     }
     
 }
 
 class Hero: Character {
-    var spells: [String] = ["Fire", "Water", "Earth", "Healing"]
+   // var spells: [String] = ["Fire", "Water", "Earth", "Healing"]
     
     init(health: Int, energy: Int) {
-        let num = Int.random(in: 1...4)
+       // let num = Int.random(in: 1...4)
         
-        super.init(health: 100, energy: 100, attackDamage: 10, spellCasted:spells[num])
+        super.init(health: 100, energy: 100, attackDamage: 10)
         
     }
 }
 
 class Enemy: Character {
     
-    var spells: [String] = ["Fire", "Death", "Poison", "Ice"]
+   // var spells: [String] = ["Fire", "Death", "Poison", "Ice"]
     
     init(health: Int, energy: Int) {
-        let num = Int.random(in: 1...4)
+        //let num = Int.random(in: 1...4)
         
-        super.init(health: 100, energy: 100, attackDamage: 10, spellCasted: spells[num])
+        super.init(health: 100, energy: 100, attackDamage: 10)
         
         }
     
