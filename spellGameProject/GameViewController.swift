71908663ad79716e72c053c47720df742e53daa5
Aly Muhammad Damani <alymuhammad.damani.144@k12.friscoisd.org>
2019-12-17 13:23:35 -0600

    energy regeneration

--- spellGameProject/GameViewController.swift
+++ spellGameProject/GameViewController.swift
@@ -17,6 +17,7 @@ class GameViewController: UIViewController {
     @IBOutlet var turnDescription: UILabel!
     @IBOutlet var enemyHealth: UILabel!
     @IBOutlet var enemyEnergy: UILabel!
+    var count = 0
     var turn = true
     var enemy = Enemy(health: 100, energy: 100)
     var player = Hero(health: 100, energy: 100)
@@ -148,6 +149,12 @@ class GameViewController: UIViewController {
         enemyHealth.text = "\(enemy.health)"
         enemyEnergy.text = "\(enemy.energy)"
         turn = true
+    }
+    func energyRegenPlayer() {
+        player.energy += 10
+    }
+    func energyRegenEnemy() {
+        enemy.energy += 10
     }
     override var shouldAutorotate: Bool {
         return true
