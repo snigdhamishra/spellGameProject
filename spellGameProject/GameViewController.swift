eb8de9375dbe1f21f8852b9b234fb375fedc286c
Nicholas Sung <nicholas.sung.332@k12.friscoisd.org>
2019-12-17 14:06:36 -0600

    stuffs

--- spellGameProject/GameViewController.swift
+++ spellGameProject/GameViewController.swift
@@ -56,6 +56,10 @@ class GameViewController: UIViewController {
             player.energy -= 30
             effect.image = UIImage(named: "earth")
             playerTurn.text = "The player attacked the enemy for 10 damage!"
+            if (enemy.health <= 0)
+            {
+                superEnemy()
+            }
         }
         else
         {
@@ -78,16 +82,20 @@ class GameViewController: UIViewController {
     @IBAction func spell2(_ sender: Any) {
         turn = false
         var damage = Int.random(in: 1...20)
-        if (player.energy >= 20)
+        if (player.energy >= 10)
         {
             enemy.health -= damage
-            player.energy -= 15
+            player.energy -= 10
             if (player.energy < 0)
             {
                 player.energy = 0
             }
             effect.image = UIImage(named: "water")
             playerTurn.text = "The player attacked the enemy for \(damage) damage!"
+            if (enemy.health <= 0)
+            {
+                superEnemy()
+            }
         }
         else
         {
@@ -116,6 +124,10 @@ class GameViewController: UIViewController {
             }
             effect.image = UIImage(named: "alphacChadMove")
         playerTurn.text = "The player attacked the enemy for \(damage) damage!"
+            if (enemy.health <= 0)
+            {
+                superEnemy()
+            }
         }
         else
         {
@@ -135,7 +147,7 @@ class GameViewController: UIViewController {
         turn = false
         if (player.energy > 0)
         {
-            player.health += 10
+            player.health += 30
             player.energy -= 20
             if (player.energy < 0)
             {
@@ -143,6 +155,10 @@ class GameViewController: UIViewController {
             }
             effect.image = UIImage(named: "healing")
         playerTurn.text = "The player healed 10 health!"
+            if (enemy.health <= 0)
+            {
+                superEnemy()
+            }
             }
         else
         {
@@ -171,8 +187,11 @@ class GameViewController: UIViewController {
             }
             else
             {
+                turnDescription.text = "The enemy tried to atack the player but failed!"
+            }
+            if (enemy.energy <= 0)
+            {
                 enemy.energy = 0
-                turnDescription.text = "The enemy tried to attack the player but failed!"
             }
         }
         else if (num == 2)
@@ -185,8 +204,11 @@ class GameViewController: UIViewController {
             }
             else
             {
+                turnDescription.text = "The enemy tried to atack the player but failed!"
+            }
+            if (enemy.energy <= 0)
+            {
                 enemy.energy = 0
-                turnDescription.text = "The enemy tried to attack the player but failed!"
             }
         }
         else if (num == 3)
@@ -200,29 +222,35 @@ class GameViewController: UIViewController {
             }
             else
             {
+                turnDescription.text = "The enemy tried to atack the player but failed!"
+            }
+            if (enemy.energy <= 0)
+            {
                 enemy.energy = 0
-                turnDescription.text = "The enemy tried to attack the player but failed!"
             }
         }
         else if (num == 4)
         {
             if (enemy.energy >= 40)
             {
-                enemy.health += 30
+                enemy.health += 10
                 enemy.energy -= 50
                 turnDescription.text = "Enemy healed 30 health!"
             }
-            else
-            {
-                enemy.energy = 0
-                turnDescription.text = "The enemy tried to heal but failed!"
-            }
+                else
+                {
+                    turnDescription.text = "The enemy tried to heal but failed!"
+                }
+                if (enemy.energy <= 0)
+                {
+                    enemy.energy = 0
+                }
         }
+        regen()
         playerHealth.text = "\(player.health)"
         playerEnergy.text = "\(player.energy)"
         enemyHealth.text = "\(enemy.health)"
         enemyEnergy.text = "\(enemy.energy)"
-        regen()
         turn = true
     }
     func superEnemy()
@@ -230,7 +258,9 @@ class GameViewController: UIViewController {
         wonCounter += 1
         enemy.health += 150 * wonCounter
         enemy.energy += wonCounter * 150
+        player.health += 50
         player.energy += 150
+        turnDescription.text = "Enemy #\(wonCounter) has been slain. Enemy #\(wonCounter += 1) has appeared"
     }
     func regen() {
         player.energy += 10
